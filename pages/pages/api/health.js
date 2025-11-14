0c909db3522cfba3f49eb07bd77b353cf9cbf542
vijay marka <111419184+DeveloperEducology@users.noreply.github.com>
2025-11-14 11:44:10 +0530

    Delete pages/pages directory

--- pages/pages/api/health.js
+++
@@ -1,5 +0,0 @@
-// This is a simple API route that does nothing except say "OK".
-// This is perfect for a pinger because it's very fast and uses few resources.
-export default function handler(req, res) {
-  res.status(200).json({ status: 'ok' });
-}
