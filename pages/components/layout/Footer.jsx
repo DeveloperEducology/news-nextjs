6002d0b3c38af1fa3db26ad770c57a1a234a758b
vijay marka <111419184+DeveloperEducology@users.noreply.github.com>
2025-11-15 11:23:00 +0530

    Delete pages directory

--- pages/components/layout/Footer.jsx
+++
@@ -1,9 +0,0 @@
-export default function Footer() {
-  return (
-    <footer className="bg-gray-800 text-gray-300 py-6 mt-8">
-      <div className="container mx-auto max-w-5xl px-4 text-center">
-        &copy; {new Date().getFullYear()} తెలుగు Shorts. All rights reserved.
-      </div>
-    </footer>
-  );
-}
